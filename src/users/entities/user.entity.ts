9aef3059f386cd2aec2ad754834690a5fedd196d
este <djjd2000@naver.com>
2023-06-06 23:32:45 +0900

    Style: white-spacing

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,18 +2,18 @@ import { Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity('User')
 export class UserEntity {
-    @PrimaryColumn()
-    uuid: string;
+  @PrimaryColumn()
+  uuid: string;
 
-    @Column({ length: 30 })
-    name: string;
+  @Column({ length: 30 })
+  name: string;
 
-    @Column({ length: 30 })
-    email: string;
+  @Column({ length: 30 })
+  email: string;
 
-    @Column({ length: 30 })
-    password: string;
+  @Column({ length: 30 })
+  password: string;
 
-    @Column({ length: 60 })
-    signupVerifyToken: string;
+  @Column({ length: 60 })
+  signupVerifyToken: string;
 }
